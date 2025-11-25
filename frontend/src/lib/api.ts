7c035668a23984fd0268ed795356a44668a28dfa
CountToFour <247806@edu.p.lodz.pl>
2025-11-25 19:44:30 +0100

    change

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import {useAuthStore} from "../store/auth.ts";
-import type {EditTransactionDto, TransactionDto, CreateAccountDto, TransferDto, BudgetDto} from "./types.ts";
+import type {EditTransactionDto, TransactionDto, CreateAccountDto, TransferDto, BudgetDto, CategoryDto} from "./types.ts";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
